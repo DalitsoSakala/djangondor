705740f08df2d2eb1a5788d09c2944566e33141a
Dalitso Sakala <dalitso.1sc@gmail.com>
2023-07-23 13:24:57 +0200

    Added tags to different files

---
+++ src/djangondor/templatetags/djangondor_collection_tags.py
@@ -0,0 +1,18 @@
+from django import template
+from django.urls.resolvers import ResolverMatch
+
+register = template.Library()
+
+@register.simple_tag(name="contained")
+def contained(
+    item,
+    collection,
+    output_when_true="",
+    output_when_false="",
+):
+    return output_when_true if item in collection else output_when_false
+
+
+@register.simple_tag(name="replacestr")
+def replacestr(string: str, arg: str, replacement: str):
+    return string.replace(arg, replacement)
--- src/djangondor/templatetags/djangondor_path_tags.py
+++ src/djangondor/templatetags/djangondor_path_tags.py
@@ -56,18 +56,3 @@ def active_app(
         if context.request.resolver_match.app_name == app_name
         else output_when_false
     )
-
-
-@register.simple_tag(name="contained")
-def contained(
-    item,
-    collection,
-    output_when_true="",
-    output_when_false="",
-):
-    return output_when_true if item in collection else output_when_false
-
-
-@register.simple_tag(name="replacestr")
-def replacestr(string: str, arg: str, replacement: str):
-    return string.replace(arg, replacement)
