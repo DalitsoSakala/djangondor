4febcf95716c3b3641ae3da56df3b73983573e17
Dalitso Sakala <Dalitso.1sc@gmail.com>
2025-04-25 20:07:42 +0200

    Updated version

---
+++ djangondor/__init__.py
@@ -0,0 +1,8 @@
+__title__ = 'Djangondor'
+__version__ = '1.7.0'
+__author__ = 'Dalitso Sakala <dalitso.1sc@gmail.com>'
+__license__ = 'MIT License'
+__copyright__ = 'Copyright 2024-2025'
+
+# Version synonym
+VERSION = __version__
