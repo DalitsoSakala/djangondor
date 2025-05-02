f0ed4c9a94d83bfb98cb68c0cb73de3565c06269
Dalitso Sakala <Dalitso.1sc@gmail.com>
2025-05-03 00:00:34 +0200

    Fixed issue with json_response

--- djangondor/__init__.py
+++ djangondor/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'Djangondor'
-__version__ = '1.8.0'
+__version__ = '1.8.1'
 __author__ = 'Dalitso Sakala <dalitso.1sc@gmail.com>'
 __license__ = 'MIT License'
 __copyright__ = 'Copyright 2024-2025'
